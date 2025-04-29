736cee60a04a7773ddc915c5f0983be3fcc95497
Sergio Chulian <sergio.chulian@iesdonana.org>
2025-04-29 21:18:50 +0200

    correccion estetica en data

--- resources/js/Components/Publicacion/Data.jsx
+++ resources/js/Components/Publicacion/Data.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Show({ autor, num_comentarios, likes, fecha_publicacion, titulo, visualizaciones, url }) {
     return (
         <div className="max-w-md mx-auto mt-8 p-4 border rounded bg-white shadow dark:bg-gray-800 dark:text-white">
-            <h2 className="text-xl font-semibold mb-4">Datos de la publicación</h2>
+            <h2 className="text-xl font-semibold mb-4">Publicación</h2>
 
-            {titulo && <p className="mb-2"><strong>Título:</strong> {titulo}</p>}
+            {titulo && <p className="mb-2"> {titulo}</p>}
             <p className="mb-2"><strong>Autor:</strong> {autor}</p>
             <p className="mb-2"><strong>Comentarios:</strong> {num_comentarios?.toLocaleString()}</p>
             <p className="mb-2"><strong>Likes:</strong> {likes?.toLocaleString()}</p>
