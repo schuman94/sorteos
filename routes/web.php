63589cc7387e0ee7f1ca46448d76110b623d0b55
Sergio Chulian <sergio.chulian@iesdonana.org>
2025-05-13 21:18:34 +0200

    ordenado

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\PublicacionController;
 use App\Http\Controllers\SorteoController;
 use App\Http\Controllers\Admin\UserController;
@@ -9,15 +8,14 @@ use App\Http\Controllers\CertificadoController;
 use App\Http\Controllers\ColeccionController;
 use App\Http\Controllers\PremioController;
 use App\Http\Controllers\RuletaController;
+use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Middleware\AdminMiddleware;
-use App\Models\Coleccion;
 
 Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
-
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -30,8 +28,8 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::post('/buscar-publicacion', [PublicacionController::class, 'buscar'])->name('publicacion.buscar');
-
 Route::post('/cargar-comentarios', [PublicacionController::class, 'cargarComentarios'])->middleware('auth')->name('publicacion.comentarios');
+Route::get('/comentarios', [PublicacionController::class, 'visualizarComentarios'])->middleware('auth')->name('comentarios.visualizar');
 
 Route::get('/sorteo', function () {
     $publicacion = Session::get('publicacion');
@@ -41,23 +39,19 @@ Route::get('/sorteo', function () {
         ]);
     }
     return redirect()->route('home');
-})->name('sorteo');
-
-Route::get('/comentarios', [PublicacionController::class, 'visualizarComentarios'])->middleware('auth')->name('comentarios.visualizar');
-
-Route::post('/sorteo/iniciar', [SorteoController::class, 'iniciar'])->middleware('auth')->name('sorteo.iniciar');
+})->middleware('auth')->name('sorteo');
 
 Route::get('/sorteo-manual', function () {
     return Inertia::render('Sorteo/Manual');
 })->middleware('auth')->name('sorteo.manual');
 
-Route::post('/sorteo-manual/iniciar', [SorteoController::class, 'iniciar_manual'])->middleware('auth')->name('sorteo.manual.iniciar');
-
-
-Route::get('/historial', [SorteoController::class, 'historial'])->middleware('auth')->name('sorteo.historial');
-
-Route::get('/sorteo/{sorteo}', [SorteoController::class, 'show'])->middleware('auth')->name('sorteo.show');
-Route::delete('/sorteos/{sorteo}', [SorteoController::class, 'destroy'])->name('sorteo.destroy');
+Route::middleware('auth')->group(function () {
+    Route::post('/sorteo/iniciar', [SorteoController::class, 'iniciar'])->name('sorteo.iniciar');
+    Route::post('/sorteo-manual/iniciar', [SorteoController::class, 'iniciar_manual'])->name('sorteo.manual.iniciar');
+    Route::get('/historial', [SorteoController::class, 'historial'])->name('sorteo.historial');
+    Route::get('/sorteo/{sorteo}', [SorteoController::class, 'show'])->name('sorteo.show');
+    Route::delete('/sorteos/{sorteo}', [SorteoController::class, 'destroy'])->name('sorteo.destroy');
+});
 
 Route::middleware(['auth', AdminMiddleware::class])->group(function () {
     Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users.index');
@@ -67,14 +61,11 @@ Route::middleware(['auth', AdminMiddleware::class])->group(function () {
 
 Route::get('/certificado/{codigo}', [CertificadoController::class, 'show'])->name('certificado.show');
 
-Route::middleware('auth')->group(function () {
-    Route::post('/ruleta/guardar', [RuletaController::class, 'guardar'])->name('ruleta.guardar');
-});
-
 Route::get('/ruleta', [RuletaController::class, 'inicio'])->name('ruleta');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/ruletas', [RuletaController::class, 'index'])->name('ruletas.index');
+    Route::post('/ruleta/guardar', [RuletaController::class, 'guardar'])->name('ruleta.guardar');
     Route::post('/ruletas', [RuletaController::class, 'store'])->name('ruletas.store');
     Route::put('/ruletas/{ruleta}', [RuletaController::class, 'update'])->name('ruletas.update');
     Route::delete('/ruletas/{ruleta}', [RuletaController::class, 'destroy'])->name('ruletas.destroy');
@@ -87,11 +78,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/colecciones/{coleccion}', [ColeccionController::class, 'show'])->name('colecciones.show');
 });
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/premios', [PremioController::class, 'index'])->name('premios.index');
     Route::post('/premios', [PremioController::class, 'store'])->name('premios.store');
 });
-
 
 require __DIR__ . '/auth.php';
