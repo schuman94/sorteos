fbdf70f0eb598f1bcc5a24e29c2bb9bb2aa08a69
Sergio Chulian <sergio.chulian@iesdonana.org>
2025-06-04 13:51:44 +0200

    encolar mails

--- app/Mail/RascaPremiadoUsuario.php
+++ app/Mail/RascaPremiadoUsuario.php
@@ -9,8 +9,9 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class RascaPremiadoUsuario extends Mailable
+class RascaPremiadoUsuario extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
