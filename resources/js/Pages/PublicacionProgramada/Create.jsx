197ef8bfe9fbfad8c35d89e3945662fe712ee99e
Sergio Chulian <sergio.chulian@iesdonana.org>
2025-06-05 20:38:28 +0200

    h1 create e index

--- resources/js/Pages/PublicacionProgramada/Create.jsx
+++ resources/js/Pages/PublicacionProgramada/Create.jsx
@@ -29,9 +29,7 @@ export default function Create({ urls, coleccion }) {
             <Head title="Programar publicación" />
 
             <div className="max-w-2xl mx-auto p-6 space-y-6">
-                <h1 className="text-2xl font-bold text-gray-800 text-center">
-                    Programar publicaciones de la colección: <span className="text-[#1cc2b5]">{coleccion.nombre}</span>
-                </h1>
+                <h1 className="text-2xl font-bold text-gray-800 text-center">{coleccion.nombre}</h1>
 
 
                 <form onSubmit={handleSubmit} className="space-y-6 bg-white border p-6 rounded shadow-sm">
